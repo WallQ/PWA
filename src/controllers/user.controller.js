3f452c98169cd4c1cc6b98d2debf0b0bb2e75f96
WallQ <sergio98@outlook.pt>
2021-11-04 16:23:38 +0000

    innit controller

---
+++ src/controllers/user.controller.js
@@ -0,0 +1,16 @@
+const User = require('../models/user.model');
+
+// Create and Save a new Note
+exports.create = (req, res) => {};
+
+// Retrieve and return all notes from the database.
+exports.findAll = (req, res) => {};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {};
+
+// Delete a note with the specified noteId in the request
+exports.delete = (req, res) => {};
