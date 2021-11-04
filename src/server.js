566428212526110baa49cb3e6508ac4c7a15d398
WallQ <sergio98@outlook.pt>
2021-11-04 16:25:45 +0000

    middleware

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const hostname = process.env.HOST || '127.0.0.1';
 const port = process.env.PORT || 3000;
 
 const app = express();
+app.use(express.json());
 const db = require('./database');
 
 app.listen(port, hostname, () => {
