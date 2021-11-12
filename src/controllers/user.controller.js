a449aad43989d965f88011b8cd8cc178c24f51de
Sérgio Félix <sergio98@outlook.pt>
2021-11-12 16:23:07 +0000

    Delete src directory

--- src/controllers/user.controller.js
+++
@@ -1,7 +0,0 @@
-const User = require('../models/user.model');
-
-exports.create = (req, res) => {};
-exports.findAll = (req, res) => {};
-exports.findOne = (req, res) => {};
-exports.update = (req, res) => {};
-exports.delete = (req, res) => {};
--- src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-module.exports = (app) => {
-	const user = require('../controllers/user.controller');
-
-	app.post('/user', user.create);
-	app.get('/user', user.findAll);
-	app.get('/user/:userId', user.findOne);
-	app.put('/user/:userId', user.update);
-	app.delete('/user/:userId', user.delete);
-};
