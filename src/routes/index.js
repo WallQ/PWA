3ce982d3452c9e259ee8f0c7d72f28a84b385e6d
Sérgio Félix <sergio98@outlook.pt>
2021-11-13 18:44:25 +0000

    add route to hotel api

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,11 +1,13 @@
 const express = require('express');
 let PlayerAPI = require('./players');
+let HotelAPI = require('./hotel');
 let AuthAPI = require('./auth');
 
 function initialize() {
 	let api = express();
 
 	api.use('/team', PlayerAPI());
+	api.use('/hotel', HotelAPI());
 	api.use('/auth', AuthAPI());
 
 	return api;
