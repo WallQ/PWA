94bed58b769a34d4bc3663cff3ca3dc0cf7f7569
Sérgio Félix <sergio98@outlook.pt>
2021-11-25 04:30:11 +0000

    fix

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,98 +1,105 @@
 const express = require('express');
 const rooms = require('../components/rooms');
 const validator = require('../components/rooms/validations');
+const roles = require('../config/roles');
+const verifyJWT = require('../middlewares/verifyJWT');
+const tryDecode = require('../middlewares/tryDecode');
+const verifyROLES = require('../middlewares/verifyROLES');
 
 function RoomRouter() {
 	let router = express();
-
 	router.use(express.json({ limit: '100mb' }));
 	router.use(express.urlencoded({ limit: '100mb', extended: true }));
 
-	router.use((req, res, next) => {
-		console.log('Timer:', Date.now());
-		next();
-	});
-
-	//Tested OK
 	router
-		.route('')
-		.get((req, res, next) => {
+		.route('/')
+		.get(tryDecode, (req, res, next) => {
 			let err = validator.results(req);
 			if (!err.isEmpty()) {
 				return res.status(400).json({ errors: err.array() });
 			}
-
-			console.log(req.body);
+			let opt = req.roles?.includes(roles.ADMIN)
+				? ''
+				: '-_id name description rating address contacts languages images facilities comments url';
 			rooms
-				.findAll()
+				.findAll(opt)
 				.then((rooms) => {
-					res.status(200).send(rooms);
-					next();
+					//console.log('Rotels found -> \n', rooms);
+					res.status(200).send({
+						message: 'Rooms have been successfully found.',
+						rooms: rooms,
+					});
 				})
-				.catch((err) => {
-					res.status(404).send('Error');
-					next();
-				});
+				.catch(next);
 		})
-		.post((req, res, next) => {
-			let body = req.body;
-			rooms
-				.create(body)
-				.then(() => {
-					res.status(200);
-					res.send(body);
-					next();
-				})
-				.catch((err) => {
-					res.status(401);
-					next();
-				});
-		});
+		.post(
+			verifyJWT,
+			verifyROLES(roles.ADMIN, roles.DIRECTOR),
+			(req, res, next) => {
+				let body = req.body;
+				rooms
+					.create(body)
+					.then((hotel) => {
+						//console.log('Room created -> \n', hotel);
+						res.status(201).send({
+							message: 'Room has been created successfully.',
+							hotel: hotel,
+						});
+					})
+					.catch(next);
+			}
+		);
 
 	router
-		.route('/:roomID')
+		.route('/:roomId')
 		.get((req, res, next) => {
-			let roomID = req.params.roomID;
+			let roomId = req.params.roomId;
 			rooms
-				.findById(roomID)
-				.then((rooms) => {
-					res.status(200);
-					res.send(rooms);
-					next();
-				})
-				.catch((err) => {
-					res.status(404);
-					next();
-				});
-		})
-		.put((req, res, next) => {
-			let roomID = req.params.roomID;
-			let body = req.body;
-			rooms
-				.findByIdAndUpdate(roomID, body)
+				.findById(roomId)
 				.then((room) => {
-					res.status(200);
-					res.send(room);
-					next();
+					res.status(200).send({
+						message: 'Room has been successfully found.',
+						room: room,
+					});
 				})
-				.catch((err) => {
-					res.status(404);
-					next();
-				});
+				.catch(next);
 		})
-		.delete((req, res, next) => {
-			let roomID = req.params.roomID;
-			rooms
-				.findOneAndDelete(roomID)
-				.then(() => {
-					res.status(200).json({ msg: 'OK- DELETED' });
-					next();
-				})
-				.catch((err) => {
-					res.status(404);
-					next();
-				});
-		});
+		.put(
+			verifyJWT,
+			verifyROLES(roles.ADMIN, roles.DIRECTOR),
+			(req, res, next) => {
+				let roomId = req.params.roomId;
+				let body = req.body;
+				rooms
+					.findByIdAndUpdate(roomId, body)
+					.then((room) => {
+						res.status(200);
+						res.send(room);
+						next();
+					})
+					.catch((err) => {
+						res.status(404);
+						next();
+					});
+			}
+		)
+		.delete(
+			verifyJWT,
+			verifyROLES(roles.ADMIN, roles.DIRECTOR),
+			(req, res, next) => {
+				let roomId = req.params.roomId;
+				rooms
+					.findOneAndDelete(roomId)
+					.then(() => {
+						res.status(200).json({ msg: 'OK- DELETED' });
+						next();
+					})
+					.catch((err) => {
+						res.status(404);
+						next();
+					});
+			}
+		);
 
 	router.route('/:roomID/books').get((req, res, next) => {
 		let roomID = req.params.roomID;
