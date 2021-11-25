15bbbc75df2107591afdbf9f8843fd2297cecd8d
Sérgio Félix <sergio98@outlook.pt>
2021-11-25 01:45:16 +0000

    reformat

--- src/middlewares/verifyJWT.js
+++ src/middlewares/verifyJWT.js
@@ -1,24 +1,26 @@
 const jwt = require('jsonwebtoken');
-const config = require('../config/config')[process.env.NODE_ENV || 'development'];
+const config =
+	require('../config/config')[process.env.NODE_ENV || 'development'];
 
-exports.verifyJWT = (req, res, next) => {
+const verifyJWT = (req, res, next) => {
 	const token = req.headers['x-access-token'] || req.query['x-access-token'];
 
 	if (!token) {
 		return res.status(401).send({
-			auth: false,
-			message: 'No token provided.',
+			error: {
+				status: 401,
+				message: 'Your token provided is invalid or has expired.',
+			},
 		});
 	}
 	jwt.verify(token, config.jsonwebtoken.secret, (err, decoded) => {
 		if (err) {
-			return res
-				.status(401)
-				.send({
-					auth: false,
-					message: 'Invalid token.',
-				})
-				.end();
+			return res.status(401).send({
+				error: {
+					status: 401,
+					message: 'Your token provided is invalid or has expired.',
+				},
+			});
 		}
 
 		req.userId = decoded.id;
@@ -28,27 +30,4 @@ exports.verifyJWT = (req, res, next) => {
 	});
 };
 
-exports.tryDecode = (req, res, next) => {
-	const token = req.headers['x-access-token'] || req.query['x-access-token'];
-
-	if (!token) {
-		next();
-	} else {
-		jwt.verify(token, config.jsonwebtoken.secret, (err, decoded) => {
-			if (err) {
-				return res
-					.status(401)
-					.send({
-						auth: false,
-						message: 'Invalid token.',
-					})
-					.end();
-			}
-	
-			req.userId = decoded.id;
-			req.email = decoded.email;
-			req.roles = decoded.roles;
-			next();
-		});
-	}
-};
+module.exports = verifyJWT;
