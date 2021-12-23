9a140b3749257889cc85aa941433051974d0dddf
Sérgio Félix <sergio98@outlook.pt>
2021-12-23 15:00:39 +0000

    https

--- api/src/app.js
+++ api/src/app.js
@@ -1,11 +1,12 @@
+const cors = require("cors");
 const express = require('express');
 const router = require('./routes');
 const errorHandler = require('./middlewares/errorHandler');
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(router.initialize());
-//Handle Error404
 app.use((req, res, next) => {
 	const err = new Error("Oops! Sorry, we couldn't found you're looking for.");
 	err.status = 404;
--- api/src/server.js
+++ api/src/server.js
@@ -1,9 +1,28 @@
 const config = require('./config/config')[process.env.NODE_ENV || 'development'];
 const app = require('./app');
+const fs = require('fs');
 const http = require('http');
+const https = require('https');
 
-const server = http.Server(app);
+const options = {
+	key: fs.readFileSync('./pwa.key'),
+	cert: fs.readFileSync('./pwa.cer'),
+};
 
-server.listen(config.server.port, config.server.host, () => {
-	console.log(`Server running at http://${config.server.host}:${config.server.port}/`);
-});
+http.createServer(app).listen(
+	config.server.http.port,
+	config.server.host,
+	() => {
+		console.log(
+			`Server running at -> http://${config.server.host}:${config.server.http.port}/`
+		);
+	}
+);
+
+https
+	.createServer(options, app)
+	.listen(config.server.https.port, config.server.host, () => {
+		console.log(
+			`Server running at -> http://${config.server.host}:${config.server.https.port}/`
+		);
+	});
